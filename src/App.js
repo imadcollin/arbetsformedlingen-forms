8af6e8d3e1eb6e90afd5567a2f718989059fe3e4
imadcollin <imad.collin@gmail.com>
2021-01-21 16:48:17 +0100

    Add Axios with get-request

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  const config = {
+    headers: { "Access-Control-Allow-Origin": "*" },
+  };
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios("api/countries", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+
+      setData(result.data);
+    };
+
+    fetchData();
+  }, []);
   return (
     <div className="App">
-  
-        <h1>Hello World
-
-        </h1>
-   
+      <h1>Hello World</h1>
+      {console.log(data)}
     </div>
   );
 }
