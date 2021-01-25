878c14104890b117b044451dddf353a626628080
imadcollin <imad.collin@gmail.com>
2021-01-25 17:08:13 +0100

    Add base64

--- src/componants/Document-uppdifter.js
+++ src/componants/Document-uppdifter.js
@@ -2,7 +2,28 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import "./shared.css";
-export const Dokumentuppgifter = ({ documentCallback }) => {
+const Dokumentuppgifter = ({ documentCallback }) => {
+  const [file, setFile] = React.useState("");
+  const [base64, setBase64] = React.useState("");
+
+  function handleUpload(event) {
+    console.log("working");
+    setFile(event.target.files[0]);
+    const selectedFile = event.target.files;
+
+    if (selectedFile.length > 0) {
+      console.log(">0");
+
+      var fileToLoad = selectedFile[0];
+      var fileReader = new FileReader();
+
+      fileReader.onload = function (fileLoadedEvent) {
+        let result = fileLoadedEvent.target.result;
+        setBase64(result);
+      };
+      fileReader.readAsDataURL(fileToLoad);
+    }
+  }
   return (
     <Formik
       initialValues={{ namn: "", telefon: "", email: "", handling: "" }}
@@ -14,11 +35,10 @@ export const Dokumentuppgifter = ({ documentCallback }) => {
           .max(15, "Must be 15 characters or less")
           .required("Required"),
         email: Yup.string().email("Invalid email address").required("Required"),
-        handling: Yup.string()
-          .max(15, "Must be 15 characters or less")
-          .optional(),
+        handling: Yup.boolean().optional(),
       })}
       onSubmit={(values) => {
+        values["base64"] = base64;
         documentCallback(values);
       }}
     >
@@ -43,13 +63,20 @@ export const Dokumentuppgifter = ({ documentCallback }) => {
           {formik.touched.email && formik.errors.email ? (
             <div className="error">{formik.errors.email}</div>
           ) : null}
-          <label htmlFor="handling">Handling</label>
 
-          <Field
+          <label htmlFor="handling">Handling</label>
+          <div id="upload-box">
+            <input type="file" onChange={handleUpload} />
+            <p>{file.name}</p>
+          </div>
+
+          {/* <Field
             id="handling"
-            type="text"
+            type="file"
+            onChange={formik.handleUpload}
+            onChange={handleUpload} 
             {...formik.getFieldProps("handling")}
-          />
+          /> */}
           {formik.touched.handling && formik.errors.handling ? (
             <div className="error">{formik.errors.handling}</div>
           ) : null}
